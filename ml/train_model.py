9e2a0938f7ee22cf93806231d9981c92e9d93a12
Viraj <viraj.vj8815@gmail.com>
2025-05-07 09:42:07 -0500

    Fix: Save trained model to model.pkl

--- ml/train_model.py
+++ ml/train_model.py
@@ -6,20 +6,20 @@ from sklearn.model_selection import train_test_split
 import pickle
 import os
 
-# Sample training data (can later replace with Snowflake query)
-data = pd.read_csv("https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv", header=None)
+# ✅ Step 1: Load sample data (Pima Indians dataset)
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+data = pd.read_csv(url, header=None)
+
 X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 
-# Split
+# ✅ Step 2: Train model
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Train
 model = RandomForestClassifier(n_estimators=100)
 model.fit(X_train, y_train)
 
-# Save model
-os.makedirs("ml", exist_ok=True)
+# ✅ Step 3: Save model
+os.makedirs("ml", exist_ok=True)  # Make sure the ml/ directory exists
 with open("ml/model.pkl", "wb") as f:
     pickle.dump(model, f)
 
