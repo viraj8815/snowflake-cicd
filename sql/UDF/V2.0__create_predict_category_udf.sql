ba044ac741104624727253fff214b9d0f7d6d386
Viraj <viraj.vj8815@gmail.com>
2025-05-08 05:25:58 -0500

    Clean repo: removed large files and reset history

--- sql/UDF/V2.0__create_predict_category_udf.sql
+++ sql/UDF/V2.0__create_predict_category_udf.sql
@@ -1,28 +1,38 @@
-CREATE OR REPLACE FUNCTION PREDICT_CATEGORY(
+CREATE OR REPLACE FUNCTION predict_category(
     ss_sales_price FLOAT,
     ss_quantity FLOAT,
     ss_ext_discount_amt FLOAT,
-    ss_net_profit FLOAT
+    ss_net_profit FLOAT,
+    d_year FLOAT,
+    d_month_seq FLOAT,
+    d_day FLOAT,
+    s_closed_date_sk FLOAT
 )
-RETURNS STRING
+RETURNS FLOAT
 LANGUAGE PYTHON
-RUNTIME_VERSION = 3.8
+RUNTIME_VERSION = '3.8'
 HANDLER = 'predict'
 IMPORTS = ('@ml_models_stage/model.pkl.gz')
 AS
 $$
-import _pickle as pickle
 import gzip
+import pickle
 
-def predict(ss_sales_price, ss_quantity, ss_ext_discount_amt, ss_net_profit):
-    with gzip.open("model.pkl.gz", "rb") as f:
-        model = pickle.load(f)
-    
+with gzip.open("model.pkl.gz", "rb") as f:
+    model = pickle.load(f)
+
+def predict(ss_sales_price, ss_quantity, ss_ext_discount_amt, ss_net_profit,
+            d_year, d_month_seq, d_day, s_closed_date_sk):
     features = [[
         ss_sales_price,
         ss_quantity,
         ss_ext_discount_amt,
-        ss_net_profit
+        ss_net_profit,
+        d_year,
+        d_month_seq,
+        d_day,
+        s_closed_date_sk
     ]]
-    return str(model.predict(features)[0])
+    prediction = model.predict(features)
+    return float(prediction[0])
 $$;
