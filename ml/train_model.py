ea06f7c41fcb442058e39f8953b75d87ac95ccbc
Viraj <viraj.vj8815@gmail.com>
2025-06-06 04:49:57 -0500

    WIP: MLOps updates before switching to stage

--- ml/train_model.py
+++ ml/train_model.py
@@ -214,3 +214,5 @@ with mlflow.start_run(run_name=run_name) as run:
     mlflow.log_artifact("ml/drift_baseline.json")
 
 print(f"✅ Final Binary Accuracy: {accuracy:.4f}, F1 Score: {f1:.4f}, Version: {version_number}")
+
+
