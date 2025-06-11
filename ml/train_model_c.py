c0c423737072bebea1f1697da38f39b0f78b5fca
Viraj <viraj.vj8815@gmail.com>
2025-06-10 20:11:39 -0500

    WIP: MLOps updates before switching to stage

--- ml/train_model_c.py
+++ ml/train_model_c.py
@@ -163,14 +163,12 @@ with mlflow.start_run(run_name=run_name) as run:
     input_example = X.head(5)
     mlflow.sklearn.log_model(model, "model", input_example=input_example, signature=signature)
 
-    mlflow.log_artifact("ml/model.pkl.gz")
-    mlflow.log_artifact("ml/metrics.json")
-    mlflow.log_artifact("ml/signature.json")
-    mlflow.log_artifact("ml/drift_baseline.json")
+   mlflow.log_artifact("ml/drift_baseline.json", artifact_path="artifacts")
+    mlflow.log_artifact("ml/metrics.json", artifact_path="artifacts")
+    mlflow.log_artifact("ml/signature.json", artifact_path="artifacts")
+    mlflow.log_artifact("ml/model.pkl.gz", artifact_path="artifacts")
 
 print(f"✅ Final Accuracy: {accuracy:.4f}, F1 Score: {f1:.4f}, Version: {version_number}")
-
-
 
 
 
